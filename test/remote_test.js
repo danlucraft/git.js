db86fc0aacdf8d9afcf30c36693ca36eb1e87f11
Daniel Lucraft <dan@fluentradical.com>
2011-01-31 11:49:30 +0000

    Added parser for .git/info/refs

--- test/remote_test.js
+++ test/remote_test.js
@@ -5,11 +5,21 @@ var createTestRepo = function() {
 }
   
 var REMOTE_TEST_DATA = {
-  infoRefs: "8c8d26e2f993c2c0112f5637cb05f06a95af34d8\trefs/heads/dbl-extract-models\n2d2d022c9955efc58bec5232116935de719efda4\trefs/heads/master\n"
+  infoRefs: {
+    data: "8c8d26e2f993c2c0112f5637cb05f06a95af34d8\trefs/heads/dbl-extract-models\n2d2d022c9955efc58bec5232116935de719efda4\trefs/heads/master\n",
+    result: {
+      "refs/heads/dbl-extract-models": "8c8d26e2f993c2c0112f5637cb05f06a95af34d8",
+      "refs/heads/master": "2d2d022c9955efc58bec5232116935de719efda4"
+    }
+  }
 }
 
 exports.Remote = {
   testParseDumbInfoRefs: function(test) {
+    test.deepEqual(
+      JsGit.Remote.parseInfoRefs(REMOTE_TEST_DATA.infoRefs.data),
+      REMOTE_TEST_DATA.infoRefs.result
+    )
     test.done()
   },
   
