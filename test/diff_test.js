bc46afad4f3783d2bd1d44167a1d813edc83ed5b
Daniel Lucraft <dan@fluentradical.com>
2011-02-06 10:23:52 +0000

    Diff display algorithm works for additions

--- test/diff_test.js
+++ test/diff_test.js
@@ -4,7 +4,7 @@ require('../lib/jsgit-server')
 
 
 exports.Diff = {
-  "diffs files correctly with additions": function(test) {
+  "diffs files correctly with additions at end": function(test) {
     var file1 = ["a", "b", "c", "d", "e", "f"].join("\n")
     var file2 = ["a", "b", "c", "d", "e", "f", "g", "h"].join("\n")
     var diff = new JsGit.Diff(file1, file2)
@@ -17,6 +17,43 @@ exports.Diff = {
           {oldIndex: 6, newIndex: 6, line: "f", type:"context"},
           {oldIndex: null, newIndex: 7, line: "g", type:"added"},
           {oldIndex: null, newIndex: 8, line: "h", type:"added"}
+        ]}])
+    test.done()
+  },
+  
+  "diffs files correctly with additions at beginning": function(test) {
+    var file1 = ["a", "b", "c", "d", "e", "f"].join("\n")
+    var file2 = ["0", "1", "a", "b", "c", "d", "e", "f"].join("\n")
+    var diff = new JsGit.Diff(file1, file2)
+    test.deepEqual(
+      diff.info, 
+      [{offset: 1, 
+        lines: [
+          {oldIndex: null, newIndex: 1, line: "0", type:"added"},
+          {oldIndex: null, newIndex: 2, line: "1", type:"added"},
+          {oldIndex: 1, newIndex: 3, line: "a", type:"context"},
+          {oldIndex: 2, newIndex: 4, line: "b", type:"context"},
+          {oldIndex: 3, newIndex: 5, line: "c", type:"context"}
+        ]}])
+    test.done()
+  },
+  
+  "diffs files correctly with additions in middle": function(test) {
+    var file1 = ["a", "b", "c", "d", "e", "f"].join("\n")
+    var file2 = ["a", "b", "c", "0", "1", "d", "e", "f"].join("\n")
+    var diff = new JsGit.Diff(file1, file2)
+    test.deepEqual(
+      diff.info, 
+      [{offset: 4, 
+        lines: [
+          {oldIndex: 1, newIndex: 1, line: "a", type:"context"},
+          {oldIndex: 2, newIndex: 2, line: "b", type:"context"},
+          {oldIndex: 3, newIndex: 3, line: "c", type:"context"},
+          {oldIndex: null, newIndex: 4, line: "0", type:"added"},
+          {oldIndex: null, newIndex: 5, line: "1", type:"added"},
+          {oldIndex: 4, newIndex: 6, line: "d", type:"context"},
+          {oldIndex: 5, newIndex: 7, line: "e", type:"context"},
+          {oldIndex: 6, newIndex: 8, line: "f", type:"context"}
         ]}])
     test.done()
   }
