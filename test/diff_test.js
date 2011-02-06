930a71837253907505e28bb48e942023644a1a03
Daniel Lucraft <dan@fluentradical.com>
2011-02-06 10:31:36 +0000

    Diff display handles some removals

--- test/diff_test.js
+++ test/diff_test.js
@@ -3,7 +3,7 @@ require('../lib/jsgit-server')
 
 
 
-exports.Diff = {
+exports.DiffAdditions = {
   "diffs files correctly with additions at end": function(test) {
     var file1 = ["a", "b", "c", "d", "e", "f"].join("\n")
     var file2 = ["a", "b", "c", "d", "e", "f", "g", "h"].join("\n")
@@ -58,4 +58,44 @@ exports.Diff = {
     test.done()
   }
 }
+
+exports.DiffRemoved = {
+  "diffs files correctly with removals at end": function(test) {
+    var file1 = ["a", "b", "c", "d", "e", "f"].join("\n")
+    var file2 = ["a", "b", "c", "d"].join("\n")
+    var diff = new JsGit.Diff(file1, file2)
+    test.deepEqual(
+      diff.info, 
+      [{offset: 5, 
+        lines: [
+          {oldIndex: 2, newIndex: 2, line: "b", type:"context"},
+          {oldIndex: 3, newIndex: 3, line: "c", type:"context"},
+          {oldIndex: 4, newIndex: 4, line: "d", type:"context"},
+          {oldIndex: 5, newIndex: null, line: "e", type:"removed"},
+          {oldIndex: 6, newIndex: null, line: "f", type:"removed"}
+        ]}])
+    test.done()
+  },
   
+  "diffs files correctly with removals at beginning": function(test) {
+    var file1 = ["a", "b", "c", "d", "e", "f"].join("\n")
+    var file2 = ["c", "d", "e", "f"].join("\n")
+    var diff = new JsGit.Diff(file1, file2)
+    test.deepEqual(
+      diff.info, 
+      [{offset: 1, 
+        lines: [
+          {oldIndex: 1, newIndex: null, line: "a", type:"removed"},
+          {oldIndex: 2, newIndex: null, line: "b", type:"removed"},
+          {oldIndex: 3, newIndex: 1, line: "c", type:"context"},
+          {oldIndex: 4, newIndex: 2, line: "d", type:"context"},
+          {oldIndex: 5, newIndex: 3, line: "e", type:"context"},
+        ]}])
+    test.done()
+  },
+  
+}
+
+
+
+
