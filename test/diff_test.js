633eb7994aa237f9a85390ebddb27b8dc81014f8
Daniel Lucraft <dan@fluentradical.com>
2011-02-06 10:18:32 +0000

    Started redoing the diff display algorithm, with tests this time

--- test/diff_test.js
+++ test/diff_test.js
@@ -8,7 +8,16 @@ exports.Diff = {
     var file1 = ["a", "b", "c", "d", "e", "f"].join("\n")
     var file2 = ["a", "b", "c", "d", "e", "f", "g", "h"].join("\n")
     var diff = new JsGit.Diff(file1, file2)
-    diff.toHtml()
+    test.deepEqual(
+      diff.info, 
+      [{offset: 7, 
+        lines: [
+          {oldIndex: 4, newIndex: 4, line: "d", type:"context"},
+          {oldIndex: 5, newIndex: 5, line: "e", type:"context"},
+          {oldIndex: 6, newIndex: 6, line: "f", type:"context"},
+          {oldIndex: null, newIndex: 7, line: "g", type:"added"},
+          {oldIndex: null, newIndex: 8, line: "h", type:"added"}
+        ]}])
     test.done()
   }
 }
