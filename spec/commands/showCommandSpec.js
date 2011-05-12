3f1140bb6e56cf3fe94bc00b3b56b2213dd4685c
Daniel Lucraft <dan@fluentradical.com>
2011-05-12 22:05:41 +0100

    Repo can now get objects from pack files as well as loose.

--- spec/commands/showCommandSpec.js
+++ spec/commands/showCommandSpec.js
@@ -2,15 +2,17 @@ require('../../lib/jsgit-server')
 var path = require('path')
 
 describe("Show a commit", function() {
-  var repo
+  var looseRepo
+  var packedRepo
 
   beforeEach(function () {
-    repo = new JsGit.Repo(path.join(__dirname, "../../test/fixtures/test-repo1"))
+    looseRepo = new JsGit.Repo(path.join(__dirname, "../../test/fixtures/test-repo1"))
+    packedRepo = new JsGit.Repo(path.join(__dirname, "../../test/fixtures/test-repo1-packed"))
   })
   
   it("should show a loose blob", function() {
     var sha = "17cd63e8471b837707bb7840e87b1772579ab784"
-    var cmd = new JsGit.commands.ShowCommand(repo, [sha])
+    var cmd = new JsGit.commands.ShowCommand(looseRepo, [sha])
     var output = null
     
     cmd.run(function(r) { output = r })
@@ -19,7 +21,22 @@ describe("Show a commit", function() {
       "Never found object", 10000)
       
     runs(function() {
-      console.log(output)
+      expect(output).not.toMatch(/^blob/)
+      expect(output).toMatch(/Just a test repo for something I'm working on/)
+    })
+  })
+  
+  it("should show a packed blob", function() {
+    var sha = "17cd63e8471b837707bb7840e87b1772579ab784"
+    var cmd = new JsGit.commands.ShowCommand(packedRepo, [sha])
+    var output = null
+    
+    cmd.run(function(r) { output = r })
+    
+    waitsFor(function() { return output },
+      "Never found object", 10000)
+      
+    runs(function() {
       expect(output).not.toMatch(/^blob/)
       expect(output).toMatch(/Just a test repo for something I'm working on/)
     })
@@ -27,7 +44,24 @@ describe("Show a commit", function() {
   
   it("should show a loose tree", function() {
     var sha = "5541389163ccf38f7138c50c691c05e790d122d1"
-    var cmd = new JsGit.commands.ShowCommand(repo, [sha])
+    var cmd = new JsGit.commands.ShowCommand(looseRepo, [sha])
+    var output = null
+    
+    cmd.run(function(r) { output = r })
+    
+    waitsFor(function() { return output },
+      "Never found object", 10000)
+      
+    runs(function() {
+      expect(output).toMatch(/tree 5541389163ccf38f7138c50c691c05e790d122d1/)
+      expect(output).toMatch(/README/)
+      expect(output).toMatch(/lib\//)
+    })
+  })
+  
+  it("should show a packed tree", function() {
+    var sha = "5541389163ccf38f7138c50c691c05e790d122d1"
+    var cmd = new JsGit.commands.ShowCommand(packedRepo, [sha])
     var output = null
     
     cmd.run(function(r) { output = r })
