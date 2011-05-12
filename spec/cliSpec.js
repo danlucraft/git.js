94f2988f965323320e62d46470b7d309a389a108
Daniel Lucraft <dan@fluentradical.com>
2011-05-12 06:30:23 +0100

    Cli dispatches show and log commands

--- spec/cliSpec.js
+++ spec/cliSpec.js
@@ -1,36 +1,45 @@
 require('../lib/jsgit-server')
 var path = require('path')
 
+var mockRunner = {show:function() {}, log:function() {}}
+
 describe("--git-dir", function() {
   it("default should be the current working directory", function() {
     var cli = new JsGit.Cli()
     
-    expect(cli.gitDir).toEqual(process.cwd())
+    expect(cli.repo.dir).toEqual(process.cwd())
   })
   
   it("can specify a relative git dir", function() {
     var cli = new JsGit.Cli(["--git-dir=.."])
     
-    expect(cli.gitDir).toEqual(path.join(process.cwd(), ".."))
+    expect(cli.repo.dir).toEqual(path.join(process.cwd(), ".."))
   })
   
   it("can specify an absolute git dir", function() {
     var test_dir = path.join(process.cwd(), "..", "..")
     var cli = new JsGit.Cli(["--git-dir=" + test_dir])
     
-    expect(cli.gitDir).toEqual(path.join(process.cwd(), "..", ".."))
+    expect(cli.repo.dir).toEqual(path.join(process.cwd(), "..", ".."))
   })
 })
 
 describe("commands", function() {
   it("should run the show command", function() {
-    var runner = {show:function() {}}
-    
-    spyOn(runner, 'show')
-    var cli = new JsGit.Cli(["show"], runner)
+    spyOn(mockRunner, 'show')
+    var cli = new JsGit.Cli(["show"], mockRunner)
     
     cli.run()
     
-    expect(runner.show).toHaveBeenCalledWith(1)
+    expect(mockRunner.show).toHaveBeenCalledWith(cli.repo)
+  })
+  
+  it("should run the log command", function() {
+    spyOn(mockRunner, 'log')
+    var cli = new JsGit.Cli(["log"], mockRunner)
+    
+    cli.run()
+    
+    expect(mockRunner.log).toHaveBeenCalledWith(cli.repo)
   })
 })
