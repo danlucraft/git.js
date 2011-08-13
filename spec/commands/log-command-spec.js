275d7cfb3a7f0823f4d31995d3a6bd08a372212e
Chris Dickinson <christopher.s.dickinson@gmail.com>
2011-08-13 01:23:28 -0500

    Fix broken spec/commands/ log test

--- spec/commands/log-command-spec.js
+++ spec/commands/log-command-spec.js
@@ -25,30 +25,43 @@ describe("git log", function() {
     packedRepo = new Repo(path.join(__dirname, "../../test/fixtures/test-repo1-packed/.git"))
   })
   
-  var expectLogOutput = 
+  var expectPackedLogOutput = 
     "commit 9f00c2ee854534a785ff01115a94a50c9961610d\n" + 
     "Author: Daniel Lucraft <dan@fluentradical.com>\n" + 
-    "Date:   Mon Jan 03 2011 07:22:59 GMT+0000 (GMT)\n" + 
+    "Date:   Mon, 03 Jan 2011 07:22:59 +0000\n" + 
     "\n" + 
     "Modifiying master\n" + 
     "\n" + 
     "commit d0a9e5b650718445b53cd1cab40d21fb3891c98a\n" + 
     "Author: Daniel Lucraft <dan@fluentradical.com>\n" + 
-    "Date:   Mon Jan 03 2011 07:14:11 GMT+0000 (GMT)\n" + 
+    "Date:   Mon, 03 Jan 2011 07:14:11 +0000\n" + 
     "\n" + 
     "Modify README\n" + 
     "\n" + 
     "commit b3453be87b70a0c5dea28aacd49cf34ddb91a8c5\n" + 
     "Author: Daniel Lucraft <dan@fluentradical.com>\n" + 
-    "Date:   Mon Dec 27 2010 12:59:13 GMT+0000 (GMT)\n" + 
+    "Date:   Mon, 27 Dec 2010 12:59:13 +0000\n" + 
     "\n" + 
     "Add sample files"
-        
+
+    var expectLooseLogOutput =
+    'commit 8e8b973cde2e6470626dedfc5d82716d1450dcda\n'+
+    'Author: Daniel Lucraft <dan@fluentradical.com>\n'+
+    'Date:   Mon, 03 Jan 2011 07:08:07 +0000\n'+
+    '\n'+
+    'Thoroughly modified the README\n'+
+    '\n'+
+    'commit b3453be87b70a0c5dea28aacd49cf34ddb91a8c5\n'+
+    'Author: Daniel Lucraft <dan@fluentradical.com>\n'+
+    'Date:   Mon, 27 Dec 2010 12:59:12 +0000\n'+
+    '\n'+
+    'Add sample files'
+
   it("should show a list of branches, with the current highlighted (loose)", function() {
     var cmd = new LogCommand(looseRepo, [])
   
     expectOutput(cmd, function(output) {
-      expect(output).toEqual(expectLogOutput)
+      expect(output).toEqual(expectLooseLogOutput)
     })
   })
 
@@ -56,7 +69,7 @@ describe("git log", function() {
     var cmd = new LogCommand(packedRepo, [])
   
     expectOutput(cmd, function(output) {
-      expect(output).toEqual(expectLogOutput)
+      expect(output).toEqual(expectPackedLogOutput)
     })
   })
 })
